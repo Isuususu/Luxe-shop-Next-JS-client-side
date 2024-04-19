f601823757d75ba9dfc7f71edadbed500c58b6f6
Isuususu <jakubb12@gmail.com>
2024-04-19 16:59:55 +0100

    Stripe endpoint fixes

--- app/api/stripe_checkout/route.js
+++ app/api/stripe_checkout/route.js
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
+export async function GET(req, res) {
   if (req.method === "POST") {
     try {
       const params = {
